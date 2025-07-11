8c507e9d0083fab6aa772c1a13dc4595defec9ab
mako <nwakwueemmanuel1@gmail.com>
2025-07-11 11:10:24 -0700

    first commit

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -2,11 +2,11 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export const readLoans = async () => {
-  const data = await fs.readFile(path.resolve('data/loans.json'), 'utf-8');
+  const data = await fs.readFile(path.resolve('../data/loans.json'), 'utf-8');
   return JSON.parse(data);
 };
 
 export const readStaff = async () => {
-  const data = await fs.readFile(path.resolve('data/staff.json'), 'utf-8');
+  const data = await fs.readFile(path.resolve('../data/staffs.json'), 'utf-8');
   return JSON.parse(data);
 };
